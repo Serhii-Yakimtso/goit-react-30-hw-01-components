73f5343734f197f6a33dbd73373c5924fc1b821f
Serhii-Yakimtso <serafyso@gmail.com>
2021-06-29 22:30:44 +0300

    add Statistics

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+
 import "./App.css";
 
 import profileUserData from "../src/db/user.json";
+import statisticalData from "../src/db/statistical-data.json";
 
 // console.log(profileUserData)
-
-// const {
-//   name,
-//   tag,
-//   location,
-//   avatar,
-//   stats: { followers, views, likes },
-// } = profileUserData;
+// console.log(statisticalData);
 
 function App() {
   return (
     <div className="App">
       <Profile profileUserData={profileUserData} />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />;
     </div>
   );
 }
