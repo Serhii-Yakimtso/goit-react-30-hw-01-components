e39f1881dad02e106cf066493e156b8c68284368
Serhii-Yakimtso <serafyso@gmail.com>
2021-06-29 21:53:03 +0300

    add <\Profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import Profile from "./components/Profile/Profile";
 import "./App.css";
 
+import profileUserData from "../src/db/user.json";
+
+// console.log(profileUserData)
+
+// const {
+//   name,
+//   tag,
+//   location,
+//   avatar,
+//   stats: { followers, views, likes },
+// } = profileUserData;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile profileUserData={profileUserData} />
+    </div>
+  );
 }
 
 export default App;
